65b47b3dbf4cce94b3179d486d7525525297da30
sanchez <dfitz360@gmail.com>
2021-09-18 20:19:12 +1000

    Added support for a button which can take an ICommand as a parameter

---
+++ Sanchez.DansUI.Runner.Blazor/Models/CustomCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Sanchez.DansUI.Runner.Blazor.Models
+{
+    public class CustomCommand : ICommand
+    {
+        protected Func<Task> _onExecute;
+        protected bool _canExecute = true;
+
+        public CustomCommand(Func<Task> onExecute)
+        {
+            _onExecute = onExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute;
+        }
+
+        public async void Execute(object parameter)
+        {
+            _canExecute = false;
+            CanExecuteChanged.Invoke(this, new EventArgs());
+
+            await _onExecute();
+
+            _canExecute = true;
+            CanExecuteChanged.Invoke(this, new EventArgs());
+        }
+    }
+}
