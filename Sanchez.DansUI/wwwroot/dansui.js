3ab7d163e39e775ae7561c253555e6b5ca0d1511
sanchez <dfitz360@gmail.com>
2021-09-25 20:10:58 +1000

    set action key as slash

--- Sanchez.DansUI/wwwroot/dansui.js
+++ Sanchez.DansUI/wwwroot/dansui.js
@@ -5,7 +5,7 @@ DansUI.commander = (function () {
 
     self.listen = function (cb) {
         window.addEventListener("keypress", function (e) {
-            if (e.shiftKey && e.key == "S") {
+            if (e.key == "/") {
                 if (document.activeElement.tagName.toLowerCase() != "input") {
                     e.stopPropagation();
                     e.preventDefault();
