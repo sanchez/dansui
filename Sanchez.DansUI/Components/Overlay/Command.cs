e8fd938e25d8ec8dd0dec98457b3e585f1734eae
sanchez <dfitz360@gmail.com>
2021-09-27 22:08:30 +1000

    Added in some example commands

--- Sanchez.DansUI/Components/Overlay/Command.cs
+++ Sanchez.DansUI/Components/Overlay/Command.cs
@@ -10,5 +10,19 @@ namespace Sanchez.DansUI.Components.Overlay
         public string Name { get; set; }
         public string Description { get; set; }
         public Action OnExecute { get; set; }
+
+        public Command() { }
+
+        public Command(IconType icon, string name, Action onExecute) : this()
+        {
+            Icon = icon;
+            Name = name;
+            OnExecute = onExecute;
+        }
+
+        public Command(IconType icon, string name, string description, Action onExecute) : this(icon, name, onExecute)
+        {
+            Description = description;
+        }
     }
 }
