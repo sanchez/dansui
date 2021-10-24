fee6a0b73beb5dbc9daa6e015847eed07377dc0d
sanchez <dfitz360@gmail.com>
2021-10-24 22:41:57 +1000

    Added more unit tests getting functionality concrete

--- Sanchez.DansUI.Tests/Extensions/FuzzyMatchTests.cs
+++ Sanchez.DansUI.Tests/Extensions/FuzzyMatchTests.cs
@@ -11,6 +11,20 @@ namespace Sanchez.DansUI.Tests.Extensions
         public static IEnumerable<object[]> FuzzyMatchTestInputs()
         {
             yield return new object[] { "hello", "he", 10 };
+            yield return new object[] { "hello", "w", -1 };
+            yield return new object[] { "hello", "lo", 7 };
+            yield return new object[] { "left", "right", -3 };
+            yield return new object[] { "Goulburn", "Gburn", 24 };
+
+            // match start: (3 + 2 + 3 + 10) = 18
+            // match end: (3 + 3 + 10) = 10
+            yield return new object[] { "lLLL", "LLL", 18 };
+
+            yield return new object[] { "alLLL", "LLL", 16 };
+            yield return new object[] { "Apple", "A", 5 };
+            yield return new object[] { "Apple", "Ae", 6 };
+            yield return new object[] { "water", "ae", 2 };
+            yield return new object[] { "water", "at", 7 };
         }
 
         [Theory]
