03444cf0a4e71237b1c6213734d2a622047cd496
Daniel Fitz <dfitz360@gmail.com>
2021-10-25 17:37:00 +1000

    #12 - fuzzy search (#21)
    Added fuzzy searching

---
+++ Sanchez.DansUI.Tests/Extensions/FuzzyMatchTests.cs
@@ -0,0 +1,38 @@
+﻿using Sanchez.DansUI.Extensions;
+
+using System.Collections.Generic;
+
+using Xunit;
+
+namespace Sanchez.DansUI.Tests.Extensions
+{
+    public class FuzzyMatchTests
+    {
+        public static IEnumerable<object[]> FuzzyMatchTestInputs()
+        {
+            yield return new object[] { "hello", "he", 10 };
+            yield return new object[] { "hello", "w", -1 };
+            yield return new object[] { "hello", "lo", 7 };
+            yield return new object[] { "left", "right", -3 };
+            yield return new object[] { "Goulburn", "Gburn", 24 };
+
+            // match start: (3 + 2 + 3 + 10) = 18
+            // match end: (3 + 3 + 10) = 10
+            yield return new object[] { "lLLL", "LLL", 18 };
+
+            yield return new object[] { "alLLL", "LLL", 16 };
+            yield return new object[] { "Apple", "A", 5 };
+            yield return new object[] { "Apple", "Ae", 6 };
+            yield return new object[] { "water", "ae", 2 };
+            yield return new object[] { "water", "at", 7 };
+        }
+
+        [Theory]
+        [MemberData(nameof(FuzzyMatchTestInputs))]
+        public void FuzzyMatchTest(string input, string pattern, int expected)
+        {
+            var actual = input.FuzzyMatch(pattern);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
--- Sanchez.DansUI/Extensions/FuzzyExtensions.cs
+++ Sanchez.DansUI/Extensions/FuzzyExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Sanchez.DansUI.Extensions
@@ -18,6 +19,59 @@ namespace Sanchez.DansUI.Extensions
             }
 
             return minValue;
+        }
+
+        public static int FuzzyMatch(this string input, string pattern)
+        {
+            if (input.Length == 0 || pattern.Length == 0) return 0;
+
+            Dictionary<char, IList<(bool IsUpper, int Position)>> inputDict = new();
+            int i = 0;
+            foreach (var c in input)
+            {
+                var lowerC = char.ToLower(c);
+                if (!inputDict.ContainsKey(lowerC))
+                    inputDict.Add(lowerC, new List<(bool IsUpper, int Position)>());
+                var isUpper = lowerC != c;
+                inputDict[lowerC].Add((isUpper, i++));
+            }
+
+            var patternProcessed = pattern.Select(x => (char.ToLower(x), char.IsUpper(x)));
+            var patternInput = new LinkedList<(char C, bool IsUpper)>(patternProcessed);
+
+            return FuzzyMatchInput(inputDict, patternInput.First, -1);
+        }
+
+        private static int FuzzyMatchInput(
+            Dictionary<char, IList<(bool IsUpper, int Position)>> inputDict,
+            LinkedListNode<(char C, bool IsUpper)> patternPosition,
+            int inputPosition)
+        {
+            if (patternPosition == null) return 0;
+
+            if (inputDict.TryGetValue(patternPosition.Value.C, out var entries))
+            {
+                var filteredEntries = entries
+                    .Where(x => x.Position > inputPosition)
+                    .Select(x =>
+                    {
+                        var matchBonus = 1; // A place to store some conditional match bonuses
+                        if (x.IsUpper && patternPosition.Value.IsUpper)
+                            matchBonus += 1; // Both upper case bonus
+                        if (inputPosition == -1 && x.Position == 0)
+                            matchBonus += 3; // Both beginning characters were matched
+                        else if (x.Position == (inputPosition + 1))
+                            matchBonus += 5; // Sequential match bonus
+                        var futureResult = FuzzyMatchInput(inputDict, patternPosition.Next, x.Position);
+                        return futureResult + matchBonus;
+                    });
+                if (filteredEntries.Any())
+                    return filteredEntries.Max();
+            }
+
+            // There were no available matches left, go to the next character in the pattern and apply a penalty
+            var skippedScore = FuzzyMatchInput(inputDict, patternPosition.Next, inputPosition);
+            return skippedScore - 1;
         }
 
         public static int LevenshteinDistance(this string a, string b)
