f9345a4ceb9815d8f546b3229b2abf341b2e8d94
sanchez <dfitz360@gmail.com>
2022-02-26 14:15:44 +1000

    Starting on treeview

---
+++ Sanchez.DansUI/Components/TreeView/ITreeItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sanchez.DansUI.Components.TreeView
+{
+    public interface ITreeItem<TChildren>
+        where TChildren : ITreeItem<TChildren>
+    {
+        bool IsExpanded { get; set; }
+
+        Task<ICollection<TChildren>> GetChildren(CancellationToken cToken);
+    }
+}
